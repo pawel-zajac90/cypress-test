fd9e719842fb99e10fc8bc09f9cfb9d5926ac980
pawel-zajac90 <zajac.pawel@protonmail.com>
2020-12-24 09:19:22 +0100

    first commit

---
+++ cypress/integration/my_tests/sample_spec_fail.js
@@ -0,0 +1,15 @@
+describe('Test with the error.', () => {
+  it('clicking "type" shows the right headings', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/something_else')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
